66e066cf6a57bec1f9a33cf5245e313c425ee8e6
Girish Thanki <girish.thanki@gmail.com>
2018-06-15 22:51:10 +0100

    Dynamic column in my table

--- MaterialTable/src/app/my-table/my-table.component.ts
+++ MaterialTable/src/app/my-table/my-table.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatSort } from '@angular/material';
-import { MyTableDataSource } from './my-table-datasource';
+import {MyTableDataSource, MyTableItem} from './my-table-datasource';
+
+
+
+
 
 @Component({
   selector: 'my-table',
@@ -15,7 +19,17 @@ export class MyTableComponent implements OnInit {
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['id', 'name'];
 
+  dynamicColumns = [
+    { columnDef: 'id',    header: 'ID',       cell: (row: MyTableItem) => `${row['id']}`        },
+    { columnDef: 'name',  header: 'Name',     cell: (row: MyTableItem) => `${row['name']}`      }
+  ];
+
+
   ngOnInit() {
     this.dataSource = new MyTableDataSource(this.paginator, this.sort);
+  }
+
+  getPropertyValue(row: MyTableItem, property: string): any {
+    return row[property];
   }
 }
