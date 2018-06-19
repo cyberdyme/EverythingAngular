faedf31273f8781401dd08c1e0b4c3117d7c6e39
Girish Thanki <girish.thanki@gmail.com>
2018-06-19 23:20:00 +0100

    Prime-ng

--- profileNg/src/app/app.module.ts
+++ profileNg/src/app/app.module.ts
@@ -10,12 +10,25 @@ import { EffectsModule } from '@ngrx/effects';
 import { AppEffects } from './app.effects';
 import {environment} from '../environments/environment.prod';
 
+import { AccordionModule } from 'primeng/primeng';
+import { PanelModule } from 'primeng/primeng';
+import { ButtonModule } from 'primeng/primeng';
+import { RadioButtonModule } from 'primeng/primeng';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AccordionModule,
+    PanelModule,
+    ButtonModule,
+    RadioButtonModule,
+    BrowserAnimationsModule,
+    FormsModule,
     AppRoutingModule,
     StoreModule.forRoot(reducers, { metaReducers }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
