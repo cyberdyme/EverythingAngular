60d9af57cadf513a43199f08a09ab1f4153878d2
cyberdyme <girish.thanki@gmail.com>
2018-06-27 17:34:06 +0100

    Update app-in-memory-api.service.ts

--- profileNg/src/app/app-in-memory-api.service.ts
+++ profileNg/src/app/app-in-memory-api.service.ts
@@ -1,18 +1,38 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { SearchModel } from './features/search/search.model';
 export class AppInMemoryApi implements InMemoryDbService {
   createDb() {
-    let heroes = [
+    const searchModel: SearchModel[] = [
       {
-        name: 'Superman',
-        description: 'Flies through the air to save damsels in distress.',
-        powers: ['flight']
+        id: '22',
+        version: 3,
+        baseKey: {
+          region: 'testRegion1',
+          undRegionGroup: 'region1',
+          swapGroup: 'testSwapGroup1',
+          modelGroup: 'modelGroup1'
+        },
+        lastUpdatedTime: '2018-04-04T10:04:46.295',
+        lastUpdatedBy: 'anyOne1',
+        reviewedBy: 'anyOne1',
+        reviewedTime: '2018-04-04T10:04:46.31'
       },
       {
-        name: 'Batman',
-        description: 'Responds stealthily to rescue citizens of Gotham from crime.',
-        powers: ['technology']
+        id: '85',
+        version: 2,
+        baseKey: {
+          region: 'testRegion2',
+          undRegionGroup: 'region2',
+          swapGroup: 'testSwapGroup2',
+          modelGroup: 'modelGroup2'
+        },
+        lastUpdatedTime: '2018-05-21T12:05:55.526',
+        lastUpdatedBy: 'anyOne2',
+        reviewedBy: 'anyOne2',
+        reviewedTime: '2018-05-21T12:05:55.528'
       }
-    ]
-    return { 'heroes': heroes };
+    ];
+
+    return { 'searchModels': searchModel };
   }
 }
