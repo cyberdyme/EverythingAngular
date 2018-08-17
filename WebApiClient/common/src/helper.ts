272b88c6514ee0fc9983bf60899331a1e2f24172
cyberdyme <girish.thanki@gmail.com>
2018-08-17 10:10:51 +0100

    Update helper.ts

--- WebApiClient/common/src/helper.ts
+++ WebApiClient/common/src/helper.ts
@@ -1,3 +1,31 @@
 export class Helper {
     
 }
+
+export function IsNumber(value: any): boolean {
+  return typeof value === 'number' && 
+    isFinite(value) && 
+    Math.floor(value) === value;
+} 
+
+export function NumberWithinRange(value: number, minValue: number, maxValue: number): boolean {
+  if(value >=minValue && value <=maxValue) {
+    return true;
+  }
+  return false;
+}
+
+export function UpperCaseFirstAllWords(str: string) : string
+{
+    if(str == null || str == undefined){
+      return str;
+    }
+
+    let pieces = str.split(" ");
+    for (let i = 0; i < pieces.length; i++ )
+    {
+        var j = pieces[i].charAt(0).toUpperCase();
+        pieces[i] = j + pieces[i].substr(1).toLowerCase();
+    }
+    return pieces.join(" ");
+}
